f6375cca30a4e9e309220898b858ed02930d04ce
syedadnan17 <159569858+syedadnan17@users.noreply.github.com>
2024-02-15 15:10:28 +0530

    Token changed TelegramBot.py

--- Telegram_bot/TelegramBot.py
+++ Telegram_bot/TelegramBot.py
@@ -7,7 +7,7 @@ import json
 
 from telegram.bot import Bot 
 
-bot = telegram.Bot(token='5923635818:AAFsDcVUx60nuzx6wHiDUJzst5F0xbwpdB0') #Replace TOKEN with your token string
+bot = telegram.Bot(token='6804027184:AAFhr7pPkC9MTDcZ3LLcifciLOEASyVB298') #Replace TOKEN with your token string
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
